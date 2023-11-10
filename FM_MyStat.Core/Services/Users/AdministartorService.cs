6ae9f65634b8e0da61edaa9b31969179fec6d2fc
SerhiyJI <serhiyji@gmail.com>
2023-11-10 19:22:34 +0200

    Add student service and enail service

---
+++ FM_MyStat.Core/Services/Users/AdministartorService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using FM_MyStat.Core.Entities.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM_MyStat.Core.Services.Users
+{
+    public class AdministartorService
+    {
+        private readonly UserManager<Administrator> _userManager;
+        private readonly SignInManager<Administrator> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly EmailService _emailService;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        public AdministartorService(
+                UserManager<Administrator> userManager,
+                SignInManager<Administrator> signInManager,
+                RoleManager<IdentityRole> roleManager,
+                EmailService emailService,
+                IMapper mapper,
+                IConfiguration configuration
+            )
+        {
+            this._signInManager = signInManager;
+            this._userManager = userManager;
+            this._roleManager = roleManager;
+            this._emailService = emailService;
+            this._mapper = mapper;
+            this._configuration = configuration;
+        }
+        
+    }
+}
