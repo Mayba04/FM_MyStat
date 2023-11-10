74ace9450dde6a0faff36ec3753a45d2c86ef573
Sost_sg54@student.itstep.org <Sost_sg54@student.itstep.org>
2023-11-10 20:06:18 +0200

    Add studentDTO

---
+++ FM_MyStat.Core/DTOs/UsersDTO/Admin/AdminDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM_MyStat.Core.DTOs.UsersDTO.Admin
+{
+    internal class AdminDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ FM_MyStat.Core/DTOs/UsersDTO/Student/StudentDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM_MyStat.Core.DTOs.UsersDTO.Student
+{
+    public class StudentDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;   
+        public string Role { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Photo { get; set; } = string.Empty;
+        public int Rating { get; set; }
+        public int GroupId { get; set; }
+    }
+}
