d5ea0d096545359e6449648f72dbb60dcdb6a7b7
Yura Bortnik <Bort_lx45@student.itstep.org>
2023-11-10 18:44:46 +0200

    Update program.cs and Add service extensions for Core and Insfrastracture files

---
+++ FM_MyStat.Core/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM_MyStat.Core
+{
+    public static class ServiceExtensions
+    {
+        public static void AddCoreServices(this IServiceCollection services)
+        {
+            
+        }
+    }
+}
---
+++ FM_MyStat.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,48 @@
+﻿using FM_MyStat.Core.Entities.Users;
+using FM_MyStat.Core.Interfaces;
+using FM_MyStat.Infrastructure.Context;
+using FM_MyStat.Infrastructure.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM_MyStat.Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AppDBContext>(opt =>
+            {
+                opt.UseSqlServer(connectionString);
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+        }
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<AppDBContext>()
+            .AddDefaultTokenProviders();
+        }
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
