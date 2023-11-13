63a769569222b3c8aee708f9986fa4a03b5edd40
SerhiyJI <serhiyji@gmail.com>
2023-11-13 18:41:21 +0200

    Satrt to Group

---
+++ FM_MyStat.Core/DTOs/GrouopsDTO/EditGroupDTO.cs
@@ -0,0 +1,19 @@
+﻿using FM_MyStat.Core.Entities.Users;
+using FM_MyStat.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM_MyStat.Core.DTOs.GrouopsDTO
+{
+    public class EditGroupDTO
+    {
+        public string Name { get; set; }
+        public Teacher Teacher { get; set; }
+        public IEnumerable<Student> Students { get; set; }
+        public IEnumerable<Homework> Homeworks { get; set; }
+    }
+}
