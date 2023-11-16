ab3111eab00e1eddb4aa3205a717830e9b1e6981
SerhiyJI <serhiyji@gmail.com>
2023-11-16 10:25:47 +0200

    Save commit

--- FM_MyStat.Web/Controllers/LoginController.cs
+++ FM_MyStat.Web/Controllers/LoginController.cs
@@ -9,12 +9,20 @@ using FM_MyStat.Core.Validation.User;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace FM_MyStat.Web.Controllers
 {
     public class LoginController : Controller
     {
         private readonly UserService _userService;
+        private Dictionary<string, string> RolesToControllers = new Dictionary<string, string>
+        {
+            {"Administrator", "Admin"},
+            {"Student", "Student"},
+            {"Teacher", "Teacher"}
+        };
+        private string? GetControllerForUser(string role) => RolesToControllers.GetValueOrDefault(role, null);
         public LoginController(UserService userService)
         {
             this._userService = userService;
@@ -30,46 +38,35 @@ namespace FM_MyStat.Web.Controllers
             return View(nameof(Login));
         }
 
+        [AllowAnonymous]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(UserLoginDTO model)
         {
             var validator = new UserLoginValidation();
             var validationResult = validator.Validate(model);
-            if (validationResult.IsValid)
+            if (!validationResult.IsValid)
             {
-                ServiceResponse result = await _userService.LoginUserAsync(model);
-                if (result.Success)
-                {
-                    var roleResult = await _userService.GetUserRoleAsync(model.Email);
-                    if (roleResult.Payload != null)
-                    {
-                        if (roleResult.Payload == "Administrator")
-                        {
-                            return RedirectToAction("Index", "Admin");
-                        }
-                        else if (roleResult.Payload == "Student")
-                        {
-                            return RedirectToAction("Index", "Student", model);
-                        }
-                        else if (roleResult.Payload == "Teacher")
-                        {
-                            return RedirectToAction("Index", "Teacher", model);
-                        }
-                        else 
-                        {
-                            ViewBag.AuthError = result.Message;
-                            return View(model);
-                        }
-                    }
-                   
-                }
-
+                ViewBag.AuthError = validationResult.Errors[0];
+                return View(model);
+            }
+            ServiceResponse result = await _userService.LoginUserAsync(model);
+            if (!result.Success)
+            {
                 ViewBag.AuthError = result.Message;
                 return View(model);
             }
-            ViewBag.AuthError = validationResult.Errors[0];
+            var roleResult = await _userService.GetUserRoleAsync(model.Email);
+            if (roleResult.Payload != null)
+            {
+                string? controller = GetControllerForUser((string)roleResult.Payload);
+                if (controller != null)
+                {
+                    return RedirectToAction("Index", controller);
+                }
+            }
+            ViewBag.AuthError = result.Message;
             return View(model);
-
         }
     }
 }
