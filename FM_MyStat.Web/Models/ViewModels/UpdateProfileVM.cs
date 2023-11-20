855731e2c9b1743a7ee165b142397f2db10171c4
Pasha Mayba <pashamayba7@gmail.com>
2023-11-20 21:00:43 +0200

    Added Profile

---
+++ FM_MyStat.Web/Models/ViewModels/UpdateProfileVM.cs
@@ -0,0 +1,11 @@
+﻿using FM_MyStat.Core.DTOs.UsersDTO.User;
+
+namespace FM_MyStat.Web.Models.ViewModels
+{
+    public class UpdateProfileVM
+    {
+        public EditUserDTO UserInfo { get; set; }
+
+        public EditUserPasswordDTO UserPassword { get; set; }
+    }
+}
