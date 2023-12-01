a102c58ffa830f907a9c071a47ba1cd6908e9d52
SerhiyJI <serhiyji@gmail.com>
2023-12-01 11:26:03 +0200

    Fix some

--- FM_MyStat.Web/Controllers/LessonMarkController.cs
+++ FM_MyStat.Web/Controllers/LessonMarkController.cs
@@ -1,16 +1,18 @@
 ﻿using FM_MyStat.Core.DTOs.LessonsDTO.LessonMark;
 using FM_MyStat.Core.DTOs.LessonsDTO.Lessons;
+using FM_MyStat.Core.Interfaces;
 using FM_MyStat.Core.Services.LessonServices;
 using FM_MyStat.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace FM_MyStat.Web.Controllers
 {
     public class LessonMarkController : Controller
     {
-        private readonly LessonMarkService _lessonMarkService;
+        private readonly ILessonMarkService _lessonMarkService;
 
-        public LessonMarkController(LessonMarkService lessonMarkService)
+        public LessonMarkController(ILessonMarkService lessonMarkService)
         {
             _lessonMarkService = lessonMarkService;
         }
@@ -21,11 +23,22 @@ namespace FM_MyStat.Web.Controllers
             var viewModel = new LessonMarksVM
             {
                 students = await _lessonMarkService.GetAllStudents(),
-                mark = new LessonMarkDTO(),  // Initialize an empty LessonMarkDTO
-                lesson = new LessonDTO()  // Initialize an empty LessonDTO
+                mark = new LessonMarkDTO(),
+                lesson = new LessonDTO()
             };
 
             return View(viewModel);
+        }
+
+        public async Task<IActionResult> SubmitMark(int Id)
+        {
+            LessonMarksVM model = new LessonMarksVM()
+            {
+                students = await _lessonMarkService.GetAllStudents(),
+                mark = new LessonMarkDTO(),
+                lesson = new LessonDTO()
+            };
+            return View(model);
         }
 
         [HttpPost]
